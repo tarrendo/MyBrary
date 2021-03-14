f79880adad9f485e314f6f3889fbd3f6b80199c0
tarrendo <jorge.lt03@gmail.com>
2021-03-14 15:08:44 -0500

    Final styles added.

--- routes/authors.js
+++ routes/authors.js
@@ -45,7 +45,7 @@ router.get('/:id', async (req, res) => {
     const author = await Author.findById(req.params.id);
     const books = await Book.find({ author: author.id }).limit(6).exec();
     res.render('authors/show', {
-      author,
+      author: author,
       booksByAuthor: books
     });
   } catch {
